90ebb7c087244cf7e13ada5cdd85871bebd35f3e
= <=>
2019-11-11 16:32:59 -0700

    user GET and POST methods

---
+++ dropbucket/app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . models import users
+from . serializers import userSerializer
+
+# Create your views here.
+class userList(APIView):
+    def get(self, request):
+        user1 = users.objects.all()
+        serializer = userSerializer(user1, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = userSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
